middas-0706/kilocode

fix: stop doing models.length for every model

packages/opencode/src/cli/cmd/tui/component/dialog-model.tsx
@@ -24,6 +24,17 @@ export function DialogModel(props: { providerID?: string }) {
 
   const connected = useConnected()
   const providers = createDialogProviderOptions()
+  // kilocode_change start
+  // Memoize anything that iterates all Kilo models to avoid calculating it for
+  // each Kilo model and tanking the UI at a couple hundred models
+  const kiloProvider = sync.data.provider.find((provider) => provider.id === "kilo")
+  const kiloModels = kiloProvider?.models ?? {}
+  const kiloRank = createMemo(() => new Map(
+    Object.entries(kiloModels).map(
+      ([id, info]) => [id, info.recommendedIndex ?? Infinity] as const,
+    ),
+  ))
+  // kilocode_change end
 
   const showExtra = createMemo(() => connected() && !props.providerID)
 
@@ -107,14 +118,8 @@ export function DialogModel(props: { providerID?: string }) {
             return true
           }),
           sortBy(
-            // kilocode_change start - Sort recommended models first for Kilo Gateway
-            (x) => {
-              if (x.value.providerID !== "kilo") return 0
-              const provider = sync.data.provider.find((p) => p.id === "kilo")
-              const model = provider?.models[x.value.modelID]
-              if (model?.recommendedIndex !== undefined) return model.recommendedIndex
-              return Object.keys(provider?.models ?? {}).length
-            },
+            // kilocode_change start - Sort within Recommended / Kilo Gateway
+            (x) => (x.value.providerID === "kilo" ? kiloRank().get(x.value.modelID)! : 0),
             // kilocode_change end
             (x) => x.footer !== "Free",
             (x) => x.title,

itv3/kilo-code-plus

packages/opencode/src/cli/cmd/tui/component/dialog-model.tsx
@@ -24,6 +24,19 @@ export function DialogModel(props: { providerID?: string }) {
 
   const connected = useConnected()
   const providers = createDialogProviderOptions()
+  // kilocode_change start
+  // Memoize anything that iterates all Kilo models to avoid calculating it for
+  // each Kilo model and tanking the UI at a couple hundred models
+  const kiloRank = createMemo(() => {
+    const provider = sync.data.provider.find((provider) => provider.id === "kilo")
+    const models = provider?.models ?? {}
+    return new Map(
+      Object.entries(models).map(
+        ([id, info]) => [id, info.recommendedIndex ?? Infinity] as const,
+      ),
+    )
+  })
+  // kilocode_change end
 
   const showExtra = createMemo(() => connected() && !props.providerID)
 
@@ -107,14 +120,8 @@ export function DialogModel(props: { providerID?: string }) {
             return true
           }),
           sortBy(
-            // kilocode_change start - Sort recommended models first for Kilo Gateway
-            (x) => {
-              if (x.value.providerID !== "kilo") return 0
-              const provider = sync.data.provider.find((p) => p.id === "kilo")
-              const model = provider?.models[x.value.modelID]
-              if (model?.recommendedIndex !== undefined) return model.recommendedIndex
-              return Object.keys(provider?.models ?? {}).length
-            },
+            // kilocode_change start - Sort within Recommended / Kilo Gateway
+            (x) => (x.value.providerID === "kilo" ? kiloRank().get(x.value.modelID) ?? Infinity : 0),
             // kilocode_change end
             (x) => x.footer !== "Free",
             (x) => x.title,