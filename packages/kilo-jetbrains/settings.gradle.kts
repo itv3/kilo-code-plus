0da817f2e78a7890c7e3cc3c6de68826dd2d3407
kirillk <kirillk@kilocode.ai>
2026-06-25 09:13:17 -0600

    chore(jetbrains): use IPGP snapshot

--- packages/kilo-jetbrains/settings.gradle.kts
+++ packages/kilo-jetbrains/settings.gradle.kts
@@ -7,6 +7,7 @@ include("backend")
 pluginManagement {
     includeBuild("build-tasks")
     repositories {
+        maven("https://central.sonatype.com/repository/maven-snapshots/")
         mavenCentral()
         gradlePluginPortal()
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies/")
